e5ed80f5647c3be2e124142c4e86f82a0e43b4e1
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-30 15:03:37 -0300

    Endpoints de customers finalizados

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,9 +1,13 @@
 import dayjs from "dayjs"
 import {db} from "../database/database.connection.js"
 
+function formataBday(date){
+    return dayjs(date).format('YYYY-MM-DD')
+}
+
 export async function getCustomers(req, res){
     try {
-        const customers = await db.query(`SELECT * FROM customers;`)
+        const customers = await db.query(`SELECT id, name, phone, cpf, TO_CHAR(birthday,'YYYY-MM-DD') as birthday FROM customers;`)
         res.send(customers.rows)
     } catch (err) {
         res.status(500).send(err.message)
@@ -13,8 +17,9 @@ export async function getCustomers(req, res){
 export async function getCustomersById(req, res){
     const {id} = req.params
     try {
-        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`,[id])
+        const customer = await db.query(`SELECT id, name, phone, cpf, TO_CHAR(birthday,'YYYY-MM-DD') as birthday FROM customers WHERE id=$1;`,[id])
         if(customer.rowCount === 0) return res.sendStatus(404)
+        customer
         res.send(customer.rows[0])
     } catch (err) {
         res.status(500).send(err.message)
@@ -23,11 +28,10 @@ export async function getCustomersById(req, res){
 
 export async function createCustomer(req, res){
     const {name, phone, cpf, birthday} = req.body
+    const formatBday = formataBday(birthday)
     try {
         const cpfValidation = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf])
-        if(cpfValidation.rowCount !== 0) return res.sendStatus(409)
-        const formatBday = dayjs(birthday).format('YYYY-MM-DD')
-        console.log(formatBday)
+        if(cpfValidation.rowCount !== 0) return res.sendStatus(409)       
         const newCust = await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1,$2,$3,$4);`, [name, phone, cpf, formatBday])
         res.sendStatus(201) 
     } catch (err) {
@@ -36,5 +40,16 @@ export async function createCustomer(req, res){
 }
 
 export async function updateCustomer(req, res){
-    res.send("updateCustomer")
+    const {id} = req.params
+    const {name, phone, cpf, birthday} = req.body
+    const formatBday = formataBday(birthday)
+    try {
+        const cpfValidation = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf])
+        if(cpfValidation.rowCount !== 0 && cpfValidation.rows[0].id !== Number(id)) return res.sendStatus(409)
+        const updateCust = await db.query(`UPDATE customers SET name=$1 , phone=$2 , cpf=$3 , birthday=$4 WHERE id=$5`, [name, phone, cpf, formatBday, id])
+        if(updateCust.rowCount === 0) return res.sendStatus(404)
+        res.sendStatus(201) 
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
