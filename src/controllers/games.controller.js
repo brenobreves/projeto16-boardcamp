d0dbbb1155576fe1e8bf9fa36e06d13c8c031fcb
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-29 22:58:08 -0300

    Endpoints da tabela games finalizados

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -10,5 +10,13 @@ export async function getGames(req, res){
 }
 
 export async function createGame(req, res){
-    res.send("createGame")
+    const {name, image, stockTotal, pricePerDay} = req.body
+    try {
+        const nameValidation = await db.query(`SELECT * FROM games WHERE name=$1;`, [name])
+        if(nameValidation.rowCount !== 0) return res.sendStatus(409)
+        const newGame = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1,$2,$3,$4);`, [name, image, stockTotal, pricePerDay])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi"
 
 export const gameSchema = Joi.object({
     name: Joi.string().required(),
-    image: Joi.string(),
+    image: Joi.string().required(),
     stockTotal: Joi.number().min(1).required(),
     pricePerDay: Joi.number().min(1).required()
 })
